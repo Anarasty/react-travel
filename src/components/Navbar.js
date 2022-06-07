b9f1f0a28737a75ca7923c8605f8c60cd8ddd390
Anarasty <murloc.ninja16@gmail.com>
2022-06-07 15:21:27 +0300

    navbar done\

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Button } from './Button';
-import './Navbar.css';
+import { Button } from "./Button";
+import "./Navbar.css";
 
 const Navbar = () => {
   const [click, setClick] = useState(false);
@@ -18,14 +18,18 @@ const Navbar = () => {
     }
   };
 
-  window.addEventListener('resize', showButton);
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener("resize", showButton);
 
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
-            TRAVEL<i className="fas fa-globe-europe"></i>
+          <Link to="/" className="navbar-logo" onClick={closeMolibeMenu}>
+            TRAVEL <i className="fas fa-globe-europe"></i>
           </Link>
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
